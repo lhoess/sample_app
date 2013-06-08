d5df88d47a8e1d53daa7ea0d075c1e147957cb38
Laurence Hoess <lhoess@gmail.com>
2013-06-07 21:41:16 -0600

    Finish layout and rountes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
+  get "users/new"
 
-  get "static_pages/help"
+  match '/signup',  to: 'users#new'
 
-  get "static_pages/about"
+  root to: 'static_pages#home'
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -61,4 +65,4 @@ SampleApp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-end
+
